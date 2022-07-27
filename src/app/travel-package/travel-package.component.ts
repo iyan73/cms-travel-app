b5f5bc665ddb6bccf92a37520fe6ddca03e41680
nurdiansyah <nurdiansyah@technosoft.com.sg>
2022-07-27 18:42:48 +0700

    setup project

---
+++ src/app/travel-package/travel-package.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-travel-package',
+  templateUrl: './travel-package.component.html',
+  styleUrls: ['./travel-package.component.scss']
+})
+export class TravelPackageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
